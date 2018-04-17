ef5b14272b6a62255cc48eaf522bea18e2655d1a
Leo <leo114000@gmail.com>
2018-04-16 21:43:46 -0300

    init commit

---
+++ src/main/java/com/utn/TP4Monitor/Tp4MonitorApplication.java
@@ -0,0 +1,12 @@
+package com.utn.TP4Monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4MonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4MonitorApplication.class, args);
+	}
+}
